2f3a2effbd84fca30c3a00f93143c248dde2b4b8
NahedIssaoui <issaouinahed96@gmail.com>
2023-06-10 12:19:31 +0100

    'First Commit' 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	new file:   src/players.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentina",
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl: "https://th.bing.com/th/id/OIP.x7a1wHuzQ6PBhoeUtgULCAHaHM?pid=ImgDet&rs=1",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 36,
+    imageUrl: "https://img2.goodfon.com/wallpaper/nbig/e/1e/krishtianu-ronaldu-ronaldu-1383.jpg",
+  },
+  {
+    name: "Neymar Jr",
+    team: "Paris Saint-Germain",
+    nationality: "Brazil",
+    jerseyNumber: 10,
+    age: 29,
+    imageUrl: "https://the18.com/sites/default/files/feature-images/GettyImages-1316436024.jpg",
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 22,
+    imageUrl: "https://i.eurosport.com/2021/04/29/3123150-64018728-2560-1440.jpg",
+  },
+];
+
+export default players;
